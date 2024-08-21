9f3ea5d8dd303fe4074fbc93a8bfd0b47f62f0f7
Yash <yashchaudhari908@gmail.com>
2024-08-22 00:06:31 +0530

    asdasd

--- backend/realtimeChatServer/index.js
+++ backend/realtimeChatServer/index.js
@@ -34,7 +34,7 @@ server.on("error", onError);
 server.on("listening", onListening);
 const io = new Server(server, {
   cors: {
-    origin: ["https://www.yashportfoliohub.site", "https://biscord.site/"],
+    origin: "https://www.yashportfoliohub.site",
     credentials: true,
   },
   cookie: true,
