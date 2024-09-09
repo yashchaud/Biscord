b26ded4dd4c458666a194268b089ed309dd5fbed
Yash <yashchaudhari908@gmail.com>
2024-09-10 02:27:34 +0530

    asd

--- backend/mediasoupServer/index.js
+++ backend/mediasoupServer/index.js
@@ -10,7 +10,7 @@ const ports = 3000;
 
 var port = normalizePort(ports || "3004");
 const corsOptions = {
-  origin: "*", // Specify the origin of your frontend application
+  origin: "https://www.yashportfoliohub.site/", // Specify the origin of your frontend application
   credentials: true, // This allows cookies and credentials to be included in the requests
 };
 app.use(cors(corsOptions));
@@ -35,7 +35,8 @@ server.on("listening", onListening);
 // Initialize Socket.IO after the server is created
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://www.yashportfoliohub.site/",
+    credentials: true,
   },
 });
 Socketsetup(io);
